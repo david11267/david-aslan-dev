c679e7dcb4779de03eb81f226c06cdbf60819a2c
David Aslan <davidaslan112671@live.se>
2025-09-20 00:52:17 +0200

    fixed linting error

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -4,7 +4,7 @@ import { projects } from '@/content'
 export function ProjectsSection() {
   return (
     <section className="space-y-16">
-      {projects.map((p, i) => (
+      {projects.map((p) => (
         <div className=" backdrop-blur-xl p-8 ">
           <Project project={p} reverse={false} />
         </div>
