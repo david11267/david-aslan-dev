043f62a0d8a4d2c19d9cc0db4a41fade1ae785fb
David Aslan <David.aslan1999@gmail.com>
2025-11-25 15:55:40 +0100

    updated fetch

--- src/hooks/useGitProjects.tsx
+++ src/hooks/useGitProjects.tsx
@@ -2,21 +2,12 @@ import { useQuery } from '@tanstack/react-query'
 import type { RepoSummaryResponse } from '@/types/githubProjects'
 
 export default function useGitProjects() {
-  const apikey = import.meta.env.VITE_GITHUB_PROJECTS_API_KEY
+  const apikey = import.meta.env.VITE_GITHUB_PROJECTS_API_KEY;
 
   return useQuery({
     queryKey: ['projects'],
     queryFn: async (): Promise<RepoSummaryResponse> => {
-      const response = await fetch(
-        'https://spring-backend-a4qk4.ondigitalocean.app/api/projects',
-        {
-          method: 'POST',
-          headers: {
-            apiKey: apikey,
-          },
-          body: '',
-        },
-      )
+      const response = await fetch(`https://spring-backend-a4qk4.ondigitalocean.app/api/projects?apiKey=${apikey}`)
 
       if (!response.ok) {
         throw new Error(`HTTP error: ${response.status}`)
